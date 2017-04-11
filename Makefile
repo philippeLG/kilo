32fdb7c41cfbeec7c0e32df3100979f42e0a5af7
A0253 <philippe.le-goff@arkea.com>
2017-04-11 21:12:39 +0200

    entering raw mode

---
+++ Makefile
@@ -0,0 +1,6 @@
+CC = gcc
+
+CFLAGS = -Wall -Wextra -pedantic -std=c99
+
+kilo: kilo.c
+	$(CC) kilo.c -o kilo $(CFLAGS) 
--- kilo.c
+++ kilo.c
@@ -1,37 +1,69 @@
+/**********************************************************/
+/*                                                        */
+/*  Build Your Own Text Editor                            */
+/*  http://viewsourcecode.org/snaptoken/kilo/index.html   */
+/*                                                        */
+/**********************************************************/
+
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
-// attribut terminal d'origine
+
+/*** data ***/
+
+// terminal attributes
 struct termios orig_termios;
 
+
+/*** terminal ***/
+// error handling
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  // save original terminal attributes
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
-  
+
+  // Turn off and disable options  
+  // http://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html#turn-off-echoing 
   struct termios raw = orig_termios;
-  //Turn off echoing and canonical mode
-  raw.c_lflag &= ~(ECHO | ICANON);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_cc[VMIN] = 0; 
+  raw.c_cc[VTIME] = 1;  //timeout
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-
+/*** init ***/ 
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {            // is control character 
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
+
   return 0;
 }
