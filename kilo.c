548d28eeb3e41cc4c75afcbdace14ff5c09c8026
A0253 <philippe.le-goff@arkea.com>
2017-04-12 19:19:07 +0200

    windows size

--- kilo.c
+++ kilo.c
@@ -12,14 +12,20 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 
 /*** data ***/
-struct termios orig_termios;  // terminal attributes
-
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios; // terminal attributes
+};
+struct editorConfig E;
 
 /*** terminal ***/
 // error handling
@@ -31,18 +37,18 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
   // save original terminal attributes
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   // Turn off and disable options  
   // http://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html#turn-off-echoing 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -63,11 +69,26 @@ char editorReadKey() {
 }
 
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+
 /*** output ***/
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
@@ -99,9 +120,15 @@ void editorProcessKeypress() {
 }
 
 
-/*** init ***/ 
+/*** init ***/
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+ 
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRefreshScreen();
