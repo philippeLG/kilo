1e28bd90c2088c38a105d9ebca51f0e5a0068366
A0253 <philippe.le-goff@arkea.com>
2017-04-16 18:48:10 +0200

    refresh screen

--- kilo.c
+++ kilo.c
@@ -155,11 +155,32 @@ int editorReadKey() {
   }
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
+
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -438,8 +459,7 @@ void editorFind() {
 
   char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)",
                              editorFindCallback);
-  
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+
   if (query) {
     free(query);
   } else {
@@ -577,7 +597,6 @@ void editorRefreshScreen() {
                                             (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
-  abAppend(&ab, "\x1b[H", 3);
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
